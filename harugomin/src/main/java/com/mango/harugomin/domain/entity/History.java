6633fc0498001bd7560153150f7be09c5cd0c462
Jeongkeon <vonjobi2@gmail.com>
2020-10-27 22:43:21 +0900

    Add Entity & ERD, Class Diagram
    Entity 소스 추가하고 ERD, Class Diagram 변경하였습니다.

---
+++ harugomin/src/main/java/com/mango/harugomin/domain/entity/History.java
@@ -0,0 +1,63 @@
+package com.mango.harugomin.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.FetchType.LAZY;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "history")
+public class History extends BaseTimeEntity {
+    @Id
+    @Column(name = "post_id")
+    private Long postId;
+
+    @JsonIgnore
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    @Lob
+    private String content;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @Column(name = "post_image")
+    private String postImage;
+
+    @OneToMany(mappedBy = "post", fetch = EAGER)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "hits")
+    private int hits;
+
+    @Column(name = "comment_num")
+    private int commentNum;
+
+    public History(Post post) {
+        this.postId = post.getPostId();
+        this.user = post.getUser();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.tagName = post.getTagName();
+        this.postImage = post.getPostImage();
+        this.comments = post.getComments();
+        this.hits = post.getHits();
+        this.commentNum = post.getCommentNum();
+        this.setModifiedDate(LocalDateTime.now());
+    }
+}
---
+++ harugomin/src/main/java/com/mango/harugomin/domain/entity/Post.java
@@ -0,0 +1,106 @@
+package com.mango.harugomin.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "post")
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @JsonIgnore
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    @Lob
+    private String content;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @Column(name = "post_image")
+    private String postImage;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "post", fetch = LAZY, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "hits")
+    private int hits;
+
+    @Column(name = "comment_num")
+    private int commentNum;
+
+    @Builder
+    public Post(User user, String title, String content, String tagName, String postImage, int hits) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.tagName = tagName;
+        this.postImage = postImage;
+        this.hits = hits;
+        this.commentNum = 0;
+    }
+
+    public void update(String title, String content, String tagName, String postImage) {
+        this.title = title;
+        this.content = content;
+        this.tagName = tagName;
+        this.postImage = postImage;
+        this.setModifiedDate(LocalDateTime.now());
+    }
+
+    @Builder
+    public Post(User user, String title, String content, String tagName, String postImage) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.tagName = tagName;
+        this.postImage = postImage;
+        this.comments = new ArrayList<>();
+        this.hits = 0;
+        this.commentNum = 0;
+    }
+
+    public void addComment(Comment comment){
+        this.comments.add(comment);
+    }
+
+    public long getUserId() {
+        return user.getUserId();
+    }
+
+    public String getUserNickname(){return user.getNickname();}
+
+    public String getUserProfileImage(){return user.getProfileImage();}
+
+    public void upCommentCount(){
+        this.commentNum++;
+    }
+
+    public void downCommentCount(){
+        this.commentNum--;
+    }
+
+}
